6da2fd8fe77859493417db7015efb97153033d41
Jun gu Han <kanryu2048@unist.ac.kr>
2019-01-23 06:53:07 +0900

    Add VideoShow

---
+++ VideoShow.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+
+cap = cv.VideoCapture('images/doit.mp4') #비디오를 불러오기
+
+while(cap.isOpened()):
+    #비디오에서 프레임단위로 읽어오기
+    ret, frame = cap.read()
+
+    #한 프레임씩 출력
+    cv.imshow('frame', frame) 
+
+    #25ms 동안 키입력 대기, 프레임시간이 됨.
+    if cv.waitKey(25) & 0xFF == 27: 
+        break
+
+#모든 객체 해제
+cap.release()
+cv.destroyAllWindows()
